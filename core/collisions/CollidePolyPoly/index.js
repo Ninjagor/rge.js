fcc713380fcd6312d57eca25a2ceac07bf7bb368
Rohit Karthik <rohitismaster@gmail.com>
2024-03-22 18:25:13 -0400

    bugfix: fixed polypoly collisions

--- core/collisions/CollidePolyPoly/index.js
+++ core/collisions/CollidePolyPoly/index.js
@@ -1,43 +1,3 @@
-// export function collidePolyPoly(polygon1, polygon2, interior) {
-    // const p1 = polygon1.vertices;
-    // const p2 = polygon2.vertices;
-//     if (interior === undefined){
-//       interior = true;
-//     }
-  
-//     // go through each of the vertices, plus the next vertex in the list
-//     var next = 0;
-//     for (var current=0; current<p1.length; current++) {
-  
-//       // get next vertex in list, if we've hit the end, wrap around to 0
-//       next = current+1;
-//       if (next === p1.length) next = 0;
-
-//       let collision;
-  
-//       // get the PVectors at our current position this makes our if statement a little cleaner
-//       var vc = p1[current];    // c for "current"
-//       var vn = p1[next];       // n for "next"
-  
-//       // use these two points (a line) to compare to the other polygon's vertices using polyLine()
-//       collision = this.collideLinePoly(vc.x,vc.y,vn.x,vn.y,p2);
-//       if (collision) {
-//         console.log("Edge collision")
-//         return true
-//       };
-  
-//       //check if the either polygon is INSIDE the other
-//       if(interior === true){
-//         collision = this.collidePointPoly(p2[0].x, p2[0].y, polygon1);
-//         if (collision) return true;
-//         collision = this.collidePointPoly(p1[0].x, p1[0].y, polygon2);
-//         if (collision) return true;
-//       }
-//     }
-  
-//     return false;
-// }
-
 export function collidePolyPoly(poly1, poly2) {
   const p1 = poly1.vertices;
   const p2 = poly2.vertices;
@@ -45,15 +5,16 @@ export function collidePolyPoly(poly1, poly2) {
   let collision = false;
 
   for (let i = 0; i < p1.length; i++) {
-    if (this.collidePointPoly(p1[i].x, p1[i].y, poly2)) {
-      
+    console.log('testing collision: ', p1[i].x, p1[i].y, poly2)
+    if (this.collidePointPoly(p1[i].x+poly1.x, p1[i].y+poly1.y, poly2)) {
+
       collision = true;
       break;
     }
   }
 
   for (let i = 0; i < p2.length; i++) {
-    if (this.collidePointPoly(p2[i].x, p2[i].y, poly1)) {  
+    if (this.collidePointPoly(p2[i].x+poly2.x, p2[i].y+poly2.y, poly1)) {  
       collision = true;
       break;
     }
